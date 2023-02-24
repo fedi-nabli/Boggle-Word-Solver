a0002beb445e8eadd15c2ae9560c291559086fe0
Fedi Nabli <fedinabli@gmail.com>
2023-02-24 21:13:50 +0100

    Initialised board

--- boggle_word_solver.py
+++ boggle_word_solver.py
@@ -1,4 +1,31 @@
+class Tree():
+  def __init__(self):
+    return
+  
+  def add(self, word):
+    return
+
 def main():
-  print("Hello, World!")
+  # Initialise game board based on user input
+  board = []
+  for i in range(0, 4):
+    # append empty row
+    board.append([])
+    for j in range(0, 4):
+      board[i].append(input().strip().upper())
+
+  # print board
+  for i in range(0, 4):
+    for j in range(0, 4):
+      print(board[i][j], end=" ")
+    print()
+  
+  # load dictianry
+  dict = open("dictinary-yawl.txt", "r")
+
+  tree = Tree()
+  for line in dict:
+    word = line.rstrip().upper()
+    tree.add(word)
 
 main()
